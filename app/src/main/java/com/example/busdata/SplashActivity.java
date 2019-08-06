94015dac62621a30a4483e3e063678540db93e00
MinYeSeul <21800251@handong.edu>
2019-08-06 14:46:44 +0900

    SplashActivity가 push 되어있지 않았음

---
+++ app/src/main/java/com/example/busdata/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.busdata;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+// https://yongtech.tistory.com/100 참고
+// 로고화면 띄우는 class
+public class SplashActivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 4초동안 유지되는 첫 로고 화면
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, FindBusStation.class));
+        finish();
+
+    }
+}
